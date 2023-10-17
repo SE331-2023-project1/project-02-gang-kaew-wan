dc056317fd3c8536d55abdef580a76655630b58f
ManSmooth <40175264+ManSmooth@users.noreply.github.com>
2023-10-18 00:14:44 +0700

    remove unused imports

--- kaew-wan-backend/src/main/java/gang/kaewwan/kaewwanbackend/rest/controller/StudentController.java
+++ kaew-wan-backend/src/main/java/gang/kaewwan/kaewwanbackend/rest/controller/StudentController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
