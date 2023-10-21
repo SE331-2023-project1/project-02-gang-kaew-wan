c67dcb3aada93dd297fa0d54730552cb9efe6892
KAN KATPARK <kant.cmd@gmail.com>
2023-10-22 03:31:06 +0700

    (chore): clean

--- kaew-wan-frontend/src/stores/comment.ts
+++ kaew-wan-frontend/src/stores/comment.ts
@@ -2,15 +2,15 @@ import type { Comment } from '@/types'
 import { defineStore } from 'pinia'
 
 export const useCommentStore = defineStore('comment', {
-    state: () => ({
-        comments: null as Comment[] | null
-    }),
-    actions: {
-        setComments(comments: Comment) {
-            this.comments = comments
-        },
-        clear() {
-            this.comments = null
-        }
+  state: () => ({
+    comments: null as Comment[] | null
+  }),
+  actions: {
+    setComments(comments: Comment) {
+      this.comments = comments
+    },
+    clear() {
+      this.comments = null
     }
+  }
 })
--- kaew-wan-frontend/src/views/MyCommentView.vue
+++ kaew-wan-frontend/src/views/MyCommentView.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-
-import {useCommentStore} from "@/stores/comment";
-import {storeToRefs} from "pinia";
+import { useCommentStore } from '@/stores/comment'
+import { storeToRefs } from 'pinia'
 
 const commentStore = useCommentStore()
 const comments = storeToRefs(commentStore).comments
-
 </script>
 
-<template>
+<template></template>
 
-</template>
-
-<style scoped>
-
-</style>
+<style scoped></style>
